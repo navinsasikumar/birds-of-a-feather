fdccea51d20d18da11e094e6213e26b027d94a72
Navin Sasikumar <navinsasikumar@gmail.com>
2015-02-15 22:49:46 -0500

    Add voting ability to posts

--- assets/js/controllers/controllers.js
+++ assets/js/controllers/controllers.js
@@ -4,20 +4,26 @@
 
 var birds = angular.module('birds.controllers', ['ngRoute']);
 
-birds.controller('PostController', ['$scope', '$routeParams', 'Post',
-  function($scope, $routeParams, Post) {
-    $scope.greeting = 'Hello!';
-    $scope.items = [
-      {
-        description: 'Do a thing',
-        completed: true
-      }, {
-        description: 'Do another thing',
-        completed: false
-      }
-    ];
+birds.controller('PostController', ['$scope', '$routeParams', 'Posts', 'Post',
+  function($scope, $routeParams, Posts, Post) {
+    $scope.posts = Posts.get({});
 
-    $scope.posts = Post.get({});
+    $scope.upVote = function(post, index) {
+      var userid = post.user.id;
+      var postid = post.id;
+      Post.upVote({userid: userid, postid: postid}, function (response) {
+        $scope.posts.posts[index].votes = response.votes;
+      });
+    };
+
+    $scope.downVote = function(post, index) {
+      var userid = post.user.id;
+      var postid = post.id;
+      Post.downVote({userid: userid, postid: postid}, function (response) {
+        $scope.posts.posts[index].votes = response.votes;
+      });
+    };
+
 
     /*$scope.changePassword = function() {
       $scope.userData.email = 'test';
--- assets/js/services.js
+++ assets/js/services.js
@@ -1,9 +1,27 @@
 var birds = angular.module('birds.services', ['ngResource']);
 
-birds.factory('Post', ['$resource',
+birds.factory('Posts', ['$resource',
   function($resource){
     return $resource('posts', {}, {
       get: {method:'GET'}
     });
+  }
+]);
+
+birds.factory('Post', ['$resource',
+  function($resource) {
+    return $resource('users/:userid/posts/:postid/',
+      {userid: '@userid', postid: '@postid'},
+      {
+        upVote: {
+          method: 'PUT',
+          url: '/users/:userid/posts/:postid/votes/up'
+        },
+        downVote: {
+          method: 'PUT',
+          url: '/users/:userid/posts/:postid/votes/down'
+        }
+
+      });
   }
 ]);
